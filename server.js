348a5f25b3eca13ab72bc0bf951d70103e196fed
Rafael Raposo <orapouso@gmail.com>
2018-03-08 17:29:07 -0300

    adding localtunnel support for local testing on access_token retrieval

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const request = require('request');
 const http = require('http');
+const lt = require('localtunnel')
 const debug = require('debug')('insta:server')
 
-module.exports = (port=process.env.PORT || 5000, clientId=process.env.CLIENT_ID, clientSecret=process.env.CLIENT_SECRET, redirectURI=process.env.REDIRECT_URI) => {
+module.exports = ({port, clientId, clientSecret, subdomain}={port:process.env.PORT || 5000, clientId: process.env.CLIENT_ID, clientSecret: process.env.CLIENT_SECRET, subdomain: process.env.SUBDOMAIN}) => {
+
   let index = `
     <html>
       <head>
         <script>
           function authInsta() {
-            let instaURL = 'https://api.instagram.com/oauth/authorize/?client_id=${clientId}&redirect_uri=${redirectURI}&response_type=code&scope=basic+public_content+comments'
-            console.log('auth url', instaURL)
-            location.href = instaURL
+            
+            fetch('/auth/url')
+              .then(res => res.json())
+              .then(instaURL => {
+                instaURL = \`https://api.instagram.com/oauth/authorize/?client_id=${clientId}&redirect_uri=\${instaURL.data}&response_type=code&scope=basic+public_content+comments\`
+                console.log('auth url', instaURL)
+                location.href = instaURL
+              })
           }
-          
+
           document.addEventListener('DOMContentLoaded', () => {
             console.log('DOM loaded')
             let body = '<button onclick="authInsta()">Auth with Insta</button>'
@@ -37,13 +44,22 @@ module.exports = (port=process.env.PORT || 5000, clientId=process.env.CLIENT_ID,
     </html>
     `
 
+  let tunnel
   let server = http.createServer((req, res) => {
     debug(`request received ${req.method}`)
     
+    let statusCode = 200
+    let headers = {'Content-Type': 'text/html; charset=utf-8'}
+    let response = index
     if (req.method === 'GET') {
-      if (req.url.endsWith('/auth')) {
+      if (req.url.endsWith('/auth/url')) {
+        debug('auth URL request')
+        headers = {'Content-Type': 'application/json'}
+        response = JSON.stringify({data: getInstaURL()})
+
+      } else if (req.url.endsWith('/auth')) {
         debug('auth index request')
-      } else if (req.url.includes('succes')) {
+      } else if (req.url.includes('success')) {
         debug('success index request')
 
         let re = /code(=([^&]*))?/g
@@ -58,25 +74,54 @@ module.exports = (port=process.env.PORT || 5000, clientId=process.env.CLIENT_ID,
               debug('token response', response.body)
               server.emit('access_token', JSON.parse(response.body))
             })
+            .catch(error => console.log('REQUEST TOKEN ERROR', error))
         }
       } else {
-        res.writeHead(404)
-        return res.end()
+        statusCode = 404
+        response = 'Not found'
       }
       
       debug('auth index sent')
-      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-      return res.end(index)
+    } else {
+      debug('method not allowed', req.method, req.url)
+      statusCode = 405
+      response = 'Method not allowed'
     }
     
-    debug('not found', req.url)
-    res.writeHead(404)
-    res.end()
+    res.writeHead(statusCode, headers)
+    return res.end(response)
   })
   
-  debug(`server started on port ${port}`)
+  // once the server is up, starts a localtunnel to get the auth response
+  // from instagram
+  server.once('listening', () => {
+    debug(`server started on port ${port}`)
+    console.log('Open your browser to http://localhost:5000')
+
+    if (subdomain) {
+      tunnel = lt(port, {subdomain: subdomain}, (error, tunnel) => {
+        if (error) { debug('ERROR on creating tunnel', error) }
+        debug(`local tunnel created on url: ${tunnel.url}`)
+      })
+  
+      tunnel.on('error', (error) => {
+        debug('local tunnel error', error)
+      })
+    }
+  })
+
+  server.once('close', () => {
+    if (tunnel) {
+      tunnel.close()
+    }
+  })
+
   server.listen(port)
   return server
+
+  function getInstaURL() {
+    return `${tunnel.url}/success`
+  }
   
   function requestToken(code) {
     let options = {
@@ -85,7 +130,7 @@ module.exports = (port=process.env.PORT || 5000, clientId=process.env.CLIENT_ID,
       form: {
         client_id: clientId,
         client_secret: clientSecret,
-        redirect_uri: redirectURI,
+        redirect_uri: getInstaURL(),
         code: code,
         grant_type: 'authorization_code'
       }
